8b01cb6f4daf96a46953ebfcfa3d80895b4f08af
fidelscodes <fidelscodes@gmail.com>
2016-09-05 15:49:44 -0400

    add root to songs#index add basic listing, viewing and editing actions to SongsController add views for songs/index and songs/show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,30 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def create
+  end
+
+  def show
+    @song = set_song
+  end
+
+  def edit
+    @song = set_song
+  end
+
+
+
+  private
+
+  def set_song
+    Song.find(params[:id])
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
 end
